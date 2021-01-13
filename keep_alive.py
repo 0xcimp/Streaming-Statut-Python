099f2aeea8afd6e1c229d787b92fa0619da72524
xCryptoYz <77395856+xCryptoYz@users.noreply.github.com>
2021-01-13 14:56:36 +0100

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+
+def index():
+
+    return "Le Streaming statut est bien connecter a un compte. [DEVELOPPED BY CRYPTO]"
+
+
+def run():
+
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+
+    server = Thread(target=run)
+
+    server.start()
+
